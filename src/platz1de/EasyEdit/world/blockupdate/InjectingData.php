b5ce5f403feaf40941169a58a697b54f365611f2
RzWDev <89142186+RzWDev@users.noreply.github.com>
2024-07-25 16:36:01 +0700

    Update InjectingData.php
    Error PacketSerializer::encoder(), 0 passed

--- src/platz1de/EasyEdit/world/blockupdate/InjectingData.php
+++ src/platz1de/EasyEdit/world/blockupdate/InjectingData.php
@@ -9,35 +9,53 @@ use pocketmine\utils\Binary;
 
 class InjectingData
 {
-	private PacketSerializer $injection;
-	private int $blockCount = 0;
-	private BlockPosition $position;
+    private PacketSerializer $injection;
+    private int $blockCount = 0;
+    private BlockPosition $position;
+    private int $protocolId;
 
-	public function __construct(int $x, int $y, int $z)
-	{
-		$this->position = new BlockPosition($x, $y, $z);
-		$this->injection = PacketSerializer::encoder();
-	}
+    public function __construct(int $protocolId)
+    {
+        $this->protocolId = $protocolId;
+        $this->position = new BlockPosition(0, 0, 0);
+        $this->injection = PacketSerializer::encoder($protocolId);
+    }
 
-	public function writeBlock(int $x, int $y, int $z, int $id): void
-	{
-		$this->blockCount++;
-		$this->injection->putVarInt($x);
-		$this->injection->putUnsignedVarInt(Binary::unsignInt($y));
-		$this->injection->putVarInt($z);
-		$this->injection->putUnsignedVarInt(TypeConverter::getInstance()->getBlockTranslator()->internalIdToNetworkId($id));
-		$this->injection->putUnsignedVarInt(2); //network flag
-		$this->injection->putUnsignedVarLong(-1); //we don't have any actors
-		$this->injection->putUnsignedVarInt(0); //not synced
-	}
+    public static function encoder(int $protocolId): self
+    {
+        return new self($protocolId);
+    }
 
-	public function toProtocol(): string
-	{
-		$serializer = PacketSerializer::encoder();
-		$serializer->putBlockPosition($this->position);
-		$serializer->putUnsignedVarInt($this->blockCount);
-		$serializer->put($this->injection->getBuffer());
-		$serializer->putUnsignedVarInt(0); //we don't use the second layer
-		return $serializer->getBuffer();
-	}
+    public static function decoder(int $protocolId, string $buffer, int $offset): self
+    {
+        $instance = new self($protocolId);
+        return $instance;
+    }
+
+    public function setPosition(int $x, int $y, int $z): void
+    {
+        $this->position = new BlockPosition($x, $y, $z);
+    }
+
+    public function writeBlock(int $x, int $y, int $z, int $id): void
+    {
+        $this->blockCount++;
+        $this->injection->putVarInt($x);
+        $this->injection->putUnsignedVarInt(Binary::unsignInt($y));
+        $this->injection->putVarInt($z);
+        $this->injection->putUnsignedVarInt(TypeConverter::getInstance()->getBlockTranslator()->internalIdToNetworkId($id));
+        $this->injection->putUnsignedVarInt(2);
+        $this->injection->putUnsignedVarLong(-1);
+        $this->injection->putUnsignedVarInt(0);
+    }
+
+    public function toProtocol(): string
+    {
+        $serializer = PacketSerializer::encoder($this->protocolId);
+        $serializer->putBlockPosition($this->position);
+        $serializer->putUnsignedVarInt($this->blockCount);
+        $serializer->put($this->injection->getBuffer());
+        $serializer->putUnsignedVarInt(0);
+        return $serializer->getBuffer();
+    }
 }
